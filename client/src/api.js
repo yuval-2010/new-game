e55e2b1ff8ab51f54157df90963b1d81c6b2660a
yuval-2010 <yuval.king.2010@gmail.com>
2025-09-10 21:18:55 +0300

    Delete client directory

--- client/src/api.js
+++
@@ -1,4 +0,0 @@
-import { io } from 'socket.io-client';
-
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080';
-export const socket = io(SERVER_URL, { autoConnect: true });
